109f03b11a00f3a81cc25292a25d712038e22bf8
VaibhavButani <100180961+VaibhavButani@users.noreply.github.com>
2025-09-29 21:42:35 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,11 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}", // scans all React components
-//   ],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         georgia: ["Georgia", "serif"],
-//         nunito: ["Nunito", "sans-serif"],
-//       },
-//     },
-//   },
-//   plugins: [],
-// }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
