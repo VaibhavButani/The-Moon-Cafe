e4fe4bf48eb11ac9e3f95d78cc80a6ae693e5cdf
VaibhavButani <100180961+VaibhavButani@users.noreply.github.com>
2025-09-29 23:56:13 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000" || import.meta.env.API ;
 
 
 export default API_BASE;
