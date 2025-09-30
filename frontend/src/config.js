d38a1f4efdd1a20e432954a70a3d56e5f64a8d73
VaibhavButani <100180961+VaibhavButani@users.noreply.github.com>
2025-09-30 09:42:12 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
 // src/config.js
-const API_BASE =
-  import.meta.env.VITE_API_BASE || "https://the-moon-cafe-backend.onrender.com/";
+const API_BASE = (import.meta.env.VITE_API_BASE || "https://the-moon-cafe-backend.onrender.com")
+  .replace(/\/+$/, ""); // remove trailing slash
+
 
 console.log("API Base URL:", API_BASE);
 
