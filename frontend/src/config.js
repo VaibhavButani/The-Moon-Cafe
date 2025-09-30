bf3f79d1fa09b52425da41eb28f0125bcb27e6dd
VaibhavButani <100180961+VaibhavButani@users.noreply.github.com>
2025-09-30 09:28:32 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,7 @@
 // src/config.js
-const API_BASE = import.meta.env.VITE_API_BASE || "https://the-moon-cafe-backend.onrender.com/" ;
-consol.log(API_BASE);
+const API_BASE =
+  import.meta.env.VITE_API_BASE || "https://the-moon-cafe-backend.onrender.com/";
+
+console.log("API Base URL:", API_BASE);
 
 export default API_BASE;
