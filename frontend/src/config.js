1ad131299bc311a82571e888d80f0ac89c7e20e1
VaibhavButani <100180961+VaibhavButani@users.noreply.github.com>
2025-10-01 11:57:07 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,5 @@
 // src/config.js
-const API_BASE = (import.meta.env.VITE_API_BASE || "https://the-moon-cafe-an6r.onrender.com")
-  .replace(/\/+$/, ""); // remove trailing slash
+const API_BASE = (import.meta.env.VITE_API_BASE || "https://the-moon-cafe-an6r.onrender.com").replace(/\/+$/, ""); // remove trailing slash
 
 console.log("Welcome To The Moon Cafe:", API_BASE);
 
