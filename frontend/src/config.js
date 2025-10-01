2f800bee6513e76308bb7d20f9cc235f2a7d27ef
VaibhavButani <100180961+VaibhavButani@users.noreply.github.com>
2025-10-01 11:52:28 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,8 @@
 // src/config.js
-const API_BASE = (import.meta.env.VITE_API_BASE || "https://the-moon-cafe-backend.onrender.com")
+const API_BASE = (import.meta.env.VITE_API_BASE || "https://the-moon-cafe-an6r.onrender.com")
   .replace(/\/+$/, ""); // remove trailing slash
 
 
-console.log("Welcome To The Moon Cafe");
+console.log("Welcome To The Moon Cafe :" `API_BASE`);
 
 export default API_BASE;
